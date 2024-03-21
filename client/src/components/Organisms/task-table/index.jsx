79e467839e2b9c6dc36a0bd593536fe6b7667982
UtkarshMistpl <utkarsh@mistpl.com>
2024-03-21 13:42:45 +0530

    remove usage type column from tables

--- client/src/components/Organisms/task-table/index.jsx
+++ client/src/components/Organisms/task-table/index.jsx
@@ -19,9 +19,6 @@ const TasksTable = ({ rows, columns, keys, setUpdateId, setOpenModal, action }) 
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
-                            USAGE TYPE
-                        </th>
                         {columns.map(((value, index) => {
                             return <th scope="col" class="px-6 py-3">
                                 {value}
@@ -33,9 +30,6 @@ const TasksTable = ({ rows, columns, keys, setUpdateId, setOpenModal, action }) 
                 <tbody>
                     {rows.map((value, index) => {
                         return <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                USAGE
-                            </th>
                             {keys.map((key, i) => {
                                 return <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {value[key]}
